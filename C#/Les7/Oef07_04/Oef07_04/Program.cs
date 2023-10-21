899c60387b042ef17d87fa9b2a6720507e435ac5
ThibauGilis <r0956399@student.thomasmore.be>
2023-10-21 15:35:36 +0200

    kheb er 1 gemist

--- C#/Les7/Oef07_04/Oef07_04/Program.cs
+++ C#/Les7/Oef07_04/Oef07_04/Program.cs
@@ -1,8 +1,8 @@
 ﻿//list aanmaken en aanvullen
-List<string> lijst =  new List<string>();
+List<string> lijst = new List<string>();
 
 //staat ni inde oef ma blijkbaar geeft codegrade 20 woorden in
-for  (int i = 0; i < 20; i++)
+for (int i = 0; i < 20; i++)
 {
     lijst.Add(Console.ReadLine());
 }
@@ -22,33 +22,38 @@ foreach (string s in lijst)
     }
 }
 
-Console.WriteLine($"Het langste woord is {LangsteWoord}\nHet kortste woord is {KortsteWoord}\nDe som van de posities is {lijst.IndexOf(LangsteWoord)+lijst.IndexOf(KortsteWoord)}");
-
-// met ARRAY ipv list
+Console.WriteLine($"Het langste woord is {LangsteWoord}\nHet kortste woord is {KortsteWoord}\nDe som van de posities is {lijst.IndexOf(LangsteWoord) + lijst.IndexOf(KortsteWoord)}");
 
 
-//  array van lengte 20 aanmaken
-//string[] lijst = new string[20];
+
+
+//--------------------------------------------------------------------------------
+// met ARRAY ipv list ------------------------------------------------------------
+//--------------------------------------------------------------------------------
+
+
+/*//  array van lengte 20 aanmaken
+string[] lijst = new string[20];
 
 //  omdat codegrade een vast aantal inputs geeft kunnen we een array gebruiken
-//for (int i = 0; i < 20; i++)
-//{
-//    lijst[i] = Console.ReadLine();
-//}
-//
+for (int i = 0; i < lijst.Length; i++)
+{
+    lijst[i] = Console.ReadLine();
+}
+
 //  bepalen van het langste en kortste woord 
-//string LangsteWoord = "", KortsteWoord = lijst[0];
-//
-//foreach (string s in lijst)
-//{
-//    if (s.Length > LangsteWoord.Length)
-//    {
-//        LangsteWoord = s;
-//    }
-//    else if (s.Length < KortsteWoord.Length)
-//    {
-//        KortsteWoord = s;
-//    }
-//}
-//
-//Console.WriteLine($"Het langste woord is {LangsteWoord}\nHet kortste woord is {KortsteWoord}\nDe som van de posities is {Array.IndexOf(lijst, LangsteWoord) + Array.IndexOf(lijst, KortsteWoord)}");
+string LangsteWoord = "", KortsteWoord = lijst[0];
+
+foreach (string s in lijst)
+{
+    if (s.Length > LangsteWoord.Length)
+    {
+        LangsteWoord = s;
+    }
+    else if (s.Length < KortsteWoord.Length)
+    {
+        KortsteWoord = s;
+    }
+}
+
+Console.WriteLine($"Het langste woord is {LangsteWoord}\nHet kortste woord is {KortsteWoord}\nDe som van de posities is {Array.IndexOf(lijst, LangsteWoord) + Array.IndexOf(lijst, KortsteWoord)}");*/
